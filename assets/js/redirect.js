30b8e4b7fb42ad0e5c29b78f45f57d2c1ec88415
smockle <smockle@users.noreply.github.com>
2020-11-06 06:28:41 +0000

    Publishing Site www.smockle.com at 03005250b65eecd32a0310d37cae77c0bf6283a0 on Fri Nov  6 06:28:41 UTC 2020

---
+++ assets/js/redirect.js
@@ -0,0 +1,8 @@
+(() => {
+  if (window.location.origin.includes("localhost")) {
+    return;
+  }
+  if (window.location.origin.includes("www.smockle.com")) {
+    window.location.href = "https://blog.smockle.com" + window.location.pathname
+  }
+})();
